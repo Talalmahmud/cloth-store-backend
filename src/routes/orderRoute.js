4fa301c85e01269ac0910bd7076232f061b27386
Talal Mahmud <mahmudtalal2@gmail.com>
2024-11-12 08:45:13 +0600

    changes are added

--- src/routes/orderRoute.js
+++ src/routes/orderRoute.js
@@ -6,13 +6,13 @@ import {
   getOrderById,
   updateOrder,
 } from "../controller/orderController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 router.post("/order", createOrder);
-router.get("/order", getAllOrder);
-router.patch("/order/:id", updateOrder);
-router.get("/order/:id", getOrderById);
-
-router.delete("/order", deleteOrder);
+router.get("/order", authMiddleware("admin"), getAllOrder);
+router.patch("/order/:id", authMiddleware("admin"), updateOrder);
+router.get("/order/:id", authMiddleware("admin"), getOrderById);
+router.delete("/order", authMiddleware("admin"), deleteOrder);
 
 export default router;
